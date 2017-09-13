81d7f5a43f934686033831027289cea91b9af1f8
Ryan Kelley <rtkelley@gmail.com>
2017-09-13 19:20:15 -0400

    updated main js file, all tests passed

--- main.js
+++ main.js
@@ -7,10 +7,20 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog (name, status, color, hungry, owner) {
+  this.name = name;
+  this.status = status;
+  this.color = color;
+  this.hungry = hungry;
+  this.owner = owner;
+}
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
+
+let sadie = new Dog ('Sadie', 'normal', 'black', false, 'mason');
+let moonshine = new Dog ('Moonshine', 'normal', undefined, true, undefined);
+let atticus = new Dog ('Atticus', undefined, undefined, true, undefined);
 
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
@@ -19,7 +29,23 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human (cool) {
+  this.cool = cool;
+  this.feed = function (dog) {
+    console.log("fed dog: ", dog)
+    dog.hungry = false;
+  }
+  this.pet = function (dog) {
+    console.log("petted dog: ", dog)
+    dog.status = 'happy';
+  }
+}
+
+
+let julia = new Human(true);
+let mason = new Human(false);
+
+
 
 // Instances of Human
 // Needed: mason, julia
